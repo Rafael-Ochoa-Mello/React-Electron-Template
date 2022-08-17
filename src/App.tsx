6d589892136f770665e452aac18dc76163f1ce4a
rafaelochoamello <rafaelochoamello@gmail.com>
2022-08-17 17:05:46 -0300

    💄 Small button style control test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
      <h1>Hello World!</h1>
-     <Button title={"Teste"} bgColor={"red"} onClick={()=>alert("Teste")}/>
+     <Button title={"Teste"} bgColor={"green"} onClick={()=>alert("Teste")}/>
     </div>
   );
 }
--- src/Atomic/Atoms/Button/Button.tsx
+++ src/Atomic/Atoms/Button/Button.tsx
@@ -3,13 +3,21 @@ import styles from './Button.module.css';
 
 interface buttonProps {
     title:String,
-    bgColor: String,
     onClick: MouseEventHandler,
+    bgColor: 'blue' | 'red' | 'green'
     //children: JSX.Element | JSX.Element[]
 }
 
 export function Button({title, bgColor, onClick}:buttonProps){
     console.log(bgColor);
-    return <button onClick={onClick} className={styles.button}>{title}</button>
+    return <button onClick={onClick} className={chooseStyle(bgColor)}>{title}</button>
 }
 
+function chooseStyle(style:String){
+    switch(style){
+        case 'blue': return styles.buttonBlue
+        case 'red':  return styles.buttonRed
+        case 'green': return styles.buttonGreen
+        default: return styles.button
+    }
+}
